6f249d8861afd7250cf2658b18ef51e3bfd2f04d
Tanya Selvog <tanyaselvog@gmail.com>
2023-02-03 17:29:38 -0800

    started editing fxml

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../views/homepage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Movies, Movies, Movies App");
+        primaryStage.setScene(new Scene(root, 800, 675));
         primaryStage.show();
     }
 
