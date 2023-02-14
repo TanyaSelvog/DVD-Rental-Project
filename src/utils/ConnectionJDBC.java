2fc5f99dc24729f488264710c2440c01c98194a6
Tanya Selvog <tanyaselvog@gmail.com>
2023-02-14 11:36:13 -0800

    added Connection code from SoftwareII project (as a basis/starting point) in ConnectionJDBC

--- src/utils/ConnectionJDBC.java
+++ src/utils/ConnectionJDBC.java
@@ -1,4 +1,69 @@
 package utils;
 
+import java.sql.DriverManager;
+
 public class ConnectionJDBC {
+}
+
+/**
+    /**
+     * Protocol
+
+    private static final String protocol = "jdbc";
+    /**
+     * Database vendor
+
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    /**
+     * Name of database
+
+    private static final String databaseName = "client_schedule";
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone=SERVER"; // LOCAL
+    /**
+     * Driver reference
+
+    private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
+    private static final String userName = "sqlUser"; // Username
+    private static String password = "Passw0rd!"; // Password
+    /**
+     * Connection interface
+
+    public static Connection connection = null;
+
+    /**
+     * Starts the database connection
+     * @return DB connection
+
+    public static Connection openConnection()
+    {
+        try {
+            Class.forName(driver); // Locate Driver
+            connection = DriverManager.getConnection(jdbcUrl, userName, password); // Reference Connection object
+
+
+        }
+
+        catch(Exception e)
+        {
+            System.out.println("Error:" + e.getMessage());
+        }
+        return connection;
+    }
+
+    /**
+     * Closes database connection
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+            System.out.println("Connection closed!");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+
+}
 }
