188565bdea9c40d300f1c012d24ae09f17382fda
Tanya Selvog <tanyaselvog@gmail.com>
2023-02-19 10:31:33 -0800

    working on ConnectionJDBC & getting the connection correct for the PostgreSQL DB

--- src/utils/ConnectionJDBC.java
+++ src/utils/ConnectionJDBC.java
@@ -6,28 +6,30 @@ import java.sql.DriverManager;
 public class ConnectionJDBC {
 
 
-    private static final String protocol = "jdbc";
 
- private static final String vendor = ":postgresql";
- private static final String location = "//localhost/";
 
- private final String url = "jdbc:postgresql://localhost/dvdrental";
- private final String user = "postgres";
- private final String password = "1892
+ private static final String url = "jdbc:postgresql://localhost/dvdrental";
+ private static final String user = "postgres";
+ private static final String password = "1892";
 
 
 
     private static final String databaseName = "dvdrental";
-    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone=SERVER"; // LOCAL
 
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
 
-    private static final String driver = "com.mysql.cj.jdbc.Driver"; // Driver reference
-    private static final String userName = "sqlUser"; // Username
-   // private static String password = "Passw0rd!"; // Password
+        }
 
-    public static Connection connection = null;
+        return conn;
+    }
 
-
+/**
     public static Connection openConnection()
     {
         try {
@@ -58,4 +60,7 @@ public class ConnectionJDBC {
 
 }
 }
+ */
+}
+
 
