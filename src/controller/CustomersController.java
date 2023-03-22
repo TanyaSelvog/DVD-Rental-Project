1e70be24f91553708e44d34523b1080242e1d6f9
Tanya Selvog <tanyaselvog@gmail.com>
2023-03-22 10:36:27 -0700

    added tablecolumn data to CustomersController

--- src/controller/CustomersController.java
+++ src/controller/CustomersController.java
@@ -1,4 +1,25 @@
 package controller;
 
-public class CustomersController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CustomersController implements Initializable {
+
+    public TableColumn custID;
+    public TableColumn firstName;
+    public TableColumn lastName;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        custID.setCellValueFactory(new PropertyValueFactory<>("customerID"));
+        firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+    }
+
+
 }
