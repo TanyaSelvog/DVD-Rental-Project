a531c0006c0db77211fd2c2afde8f7bb0c3529f1
Tanya Selvog <tanyaselvog@gmail.com>
2023-03-23 12:07:51 -0700

    added TableColumns and started init() to MoviesController

--- src/controller/MoviesController.java
+++ src/controller/MoviesController.java
@@ -1,4 +1,23 @@
 package controller;
 
-public class MoviesController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoviesController implements Initializable {
+
+    public TableColumn rating;
+    public TableColumn titleColumn;
+    public TableColumn yearColumn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
+    }
 }
