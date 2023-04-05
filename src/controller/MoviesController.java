1b024b7fbea885f8bdea026d7d4ec024a4471d12
Tanya Selvog <tanyaselvog@gmail.com>
2023-04-04 20:48:17 -0700

    added moviesList to moviesController and got list of movies to show up from database into moviesTable

--- src/controller/MoviesController.java
+++ src/controller/MoviesController.java
@@ -1,24 +1,29 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import model.Movies;
+import utils.MoviesDB;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MoviesController implements Initializable {
-
+    public TableView moviesTable;
     public TableColumn rating;
     public TableColumn titleColumn;
     public TableColumn yearColumn;
     public TextField searchTF;
 
+    private ObservableList<Movies> moviesList = MoviesDB.getMovieList();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+moviesTable.setItems(moviesList);
         rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
         titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
         yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
