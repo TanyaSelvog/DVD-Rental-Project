9c6acce275f4d494c94933cef3506be441c0b404
Tanya Selvog <tanyaselvog@gmail.com>
2023-04-04 20:21:24 -0700

    added onMoviesClick() in Controller for Movies Btn functionality

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,22 +1,40 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+private Button moviesBtn;
+private Button customersBtn;
+
+
+
+
+
+        public void onMoviesClick(ActionEvent actionEvent) throws Exception{
+
+                Parent root = FXMLLoader.load(getClass().getResource("/views/movies.fxml"));
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.setTitle("Reports");
+                Scene scene = new Scene(root, 861, 619);
+                stage.setScene(scene);
+                stage.show();
+        }
 
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
 
         }
-
-
-
-
-
 
 }
 
