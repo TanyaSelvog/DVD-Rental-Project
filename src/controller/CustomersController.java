518081b193dca6c811d165bbfdb72dc0eee3de17
Tanya Selvog <tanyaselvog@gmail.com>
2023-06-07 13:13:47 -0700

    added onHomeClick() to CustomerController

--- src/controller/CustomersController.java
+++ src/controller/CustomersController.java
@@ -1,8 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +20,10 @@ public class CustomersController implements Initializable {
     public TableColumn custID;
     public TableColumn firstName;
     public TableColumn lastName;
+    @FXML
+    private Button homeBtn;
+    @FXML
+    private Button searchBtn;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -22,4 +34,12 @@ public class CustomersController implements Initializable {
     }
 
 
+    public void onHomeClick(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/movies.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Movies that Got Made");
+        Scene scene = new Scene(root, 861, 619);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
