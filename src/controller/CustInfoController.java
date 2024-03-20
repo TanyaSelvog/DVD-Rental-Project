e55dfb8828d971b4099939bb57e12ef72a255746
Tanya Selvog <tanyaselvog@gmail.com>
2024-03-20 16:12:23 -0700

    added fxml items to CustInfoController

--- src/controller/CustInfoController.java
+++ src/controller/CustInfoController.java
@@ -1,12 +1,30 @@
 package controller;
 
 import javafx.fxml.Initializable;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 import model.Customers;
 
 public class CustInfoController implements Initializable {
+
+
+    public TextField fNameTF;
+    public TextField lastNameTF;
+
+    public TextField phoneTF;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
