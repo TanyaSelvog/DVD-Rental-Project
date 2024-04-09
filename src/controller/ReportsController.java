bc93a214f31437ca748587cc88dc16d3f0281cb5
Tanya Selvog <tanyaselvog@gmail.com>
2024-04-09 12:38:21 -0700

    added id to home btn and created method in Reports Controller

--- src/controller/ReportsController.java
+++ src/controller/ReportsController.java
@@ -1,4 +1,33 @@
 package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class ReportsController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+
+
+public class ReportsController implements Initializable {
+    public void onHomeClick(ActionEvent actionEvent) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/homepage.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Movies that Got Made");
+        Scene scene = new Scene(root, 861, 619);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
