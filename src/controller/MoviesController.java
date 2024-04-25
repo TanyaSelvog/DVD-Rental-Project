983684acc43e53baf8c9f335afa5e5d032df15be
Tanya Selvog <tanyaselvog@gmail.com>
2024-04-25 12:54:03 -0700

    added ()s in MoviesController to display table data

--- src/controller/MoviesController.java
+++ src/controller/MoviesController.java
@@ -27,6 +27,14 @@ public class MoviesController implements Initializable {
 
     private ObservableList<Movies> moviesList = MoviesDB.getMovieList();
     private ObservableList<Movies> horrorList = MoviesDB.getHorrorList();
+
+    private ObservableList<Movies> animeList = MoviesDB.getAnimationList();
+    private ObservableList<Movies> docList = MoviesDB.getDocList();
+    private ObservableList<Movies> actionList = MoviesDB.getActionList();
+    private ObservableList<Movies> classicsList = MoviesDB.getClassicsList();
+    private ObservableList<Movies> dramaList = MoviesDB.getDramaList();
+    private ObservableList<Movies> comedyList = MoviesDB.getComedyList();
+    private ObservableList<Movies> familyList = MoviesDB.getFamilyList();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 moviesTable.setItems(horrorList);
@@ -36,22 +44,43 @@ moviesTable.setItems(horrorList);
     }
 
     public void onClickClassics(ActionEvent actionEvent) {
+        moviesTable.setItems(classicsList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
         
     }
 
     public void onClickAction(ActionEvent actionEvent) {
+        moviesTable.setItems(actionList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
     }
 
     public void onClickFamily(ActionEvent actionEvent) {
+        moviesTable.setItems(familyList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
     }
 
     public void onClickComedy(ActionEvent actionEvent) {
+        moviesTable.setItems(comedyList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
     }
 
     public void onClickDrama(ActionEvent actionEvent) {
+        moviesTable.setItems(dramaList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
     }
 
     public void onSearchClick(ActionEvent actionEvent) {
+
     }
 
     public void onHomeClick(ActionEvent actionEvent) throws Exception{
@@ -63,5 +92,19 @@ moviesTable.setItems(horrorList);
         Scene scene = new Scene(root, 861, 619);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void onClickAnime(ActionEvent actionEvent) {
+        moviesTable.setItems(animeList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
+    }
+
+    public void onClickDoc(ActionEvent actionEvent) {
+        moviesTable.setItems(docList);
+        rating.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        yearColumn.setCellValueFactory(new PropertyValueFactory<>("movieYear"));
     }
 }
